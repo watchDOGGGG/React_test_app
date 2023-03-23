8fda7dc02ca2755c7ce3ba7918ab05c656d37e48
Ushie01 <abugodwinaj@gmail.com>
2023-03-23 10:11:35 +0100

    update on create account

--- src/screens/createAccount.jsx
+++ src/screens/createAccount.jsx
@@ -1,61 +1,131 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { validateSignUp } from '../utils/validateInfo';
 
 export default function CreateAccount() {
+	const [firstName, setFirstName] = useState('');
+	const [lastName, setLastName] = useState('');
+	const [email, setEmail] = useState('');
+	const [phoneNumber, setPhoneNumber] = useState('');
+	const [userName, setUserName] = useState('');
+	const [role, setRole] = useState('');
+	const [password, setPassword] = useState('');
+	const [error, setError] = useState('');
+
+	const onHandleSubmit = async (e) => {
+		e.preventDefault();
+
+		const values = {
+			firstName,
+			lastName,
+			email,
+			phoneNumber,
+			userName,
+			role,
+			password
+		}
+
+		setError(validateSignUp(values));
+
+
+
+
+	}
+
 	return (
 		<div className='flex flex-col items-center justify-center mt-12'>
 			<p className='text-7xl font-bold'>Create An Account</p>
-			<div className='w-96 shadow-lg p-4 mt-10'>
-				<div class='flex flex-col mt-5 space-y-3 font-bold'>
-					<label for='phone'>First Name</label>
+			<form className='w-96 shadow-lg p-4 mt-10'>
+				<div className='flex flex-col mt-5 space-y-3 font-bold'>
+					<label>First Name</label>
 					<input
 						type='text'
 						className='h-16 w-full border-2 rounded-2xl p-1'
+						onChange={(e) => setFirstName(e.target.value)}
 					/>
 				</div>
-				<div class='flex flex-col mt-5 space-y-3 font-bold'>
-					<label for='phone'>LastName</label>
+				{error.firstName && (
+					<p className='text-red-600 text-sm font-bold'>{error.firstName}</p>
+				)}
+
+				<div className='flex flex-col mt-5 space-y-3 font-bold'>
+					<label>LastName</label>
 					<input
 						type='text'
 						className='h-16 w-full border-2 rounded-2xl p-1'
+						onChange={(e) => setLastName(e.target.value)}
 					/>
 				</div>
-				<div class='flex flex-col mt-5 space-y-3 font-bold'>
-					<label for='phone'>Email Address</label>
+				{error.lastName && (
+					<p className='text-red-600 text-sm font-bold'>{error.lastName}</p>
+				)}
+
+				<div className='flex flex-col mt-5 space-y-3 font-bold'>
+					<label>Email Address</label>
 					<input
 						type='text'
 						className='h-16 w-full border-2 rounded-2xl p-1'
+						onChange={(e) => setEmail(e.target.value)}
 					/>
 				</div>
-				<div class='flex flex-col mt-5 space-y-3 font-bold'>
-					<label for='phone'>Phone Number</label>
+				{error.email && (
+					<p className='text-red-600 text-sm font-bold'>{error.email}</p>
+				)}
+
+				<div className='flex flex-col mt-5 space-y-3 font-bold'>
+					<label>Phone Number</label>
 					<input
 						type='text'
 						className='h-16 w-full border-2 rounded-2xl p-1'
+						onChange={(e) => setPhoneNumber(e.target.value)}
 					/>
 				</div>
-				<div class='flex flex-col mt-5 space-y-3 font-bold'>
-					<label for='phone'>User Name</label>
+				{error.phoneNumber && (
+					<p className='text-red-600 text-sm font-bold'>{error.phoneNumber}</p>
+				)}
+
+				<div className='flex flex-col mt-5 space-y-3 font-bold'>
+					<label>User Name</label>
 					<input
 						type='text'
 						className='h-16 w-full border-2 rounded-2xl p-1'
+						onChange={(e) => setUserName(e.target.value)}
 					/>
 				</div>
-				<div class='flex flex-col mt-5 space-y-3 font-bold'>
-					<label for='phone'>Role</label>
-					<select class='h-16 border-2 rounded-2xl p-1'>
-						<option value=''>Customer</option>
+				{error.userName && (
+					<p className='text-red-600 text-sm font-bold'>{error.userName}</p>
+				)}
+
+				<div className='flex flex-col mt-5 space-y-3 font-bold'>
+					<label>Role</label>
+					<select
+						type='text'
+						className='h-16 border-2 rounded-2xl p-1'
+						onClick={(e) => setRole(e.target.value)}>
+						<option value='customer'>Select a role</option>
+						<option value='customer'>Customer</option>
 						<option value='farmer'>Farmer</option>
 					</select>
 				</div>
-				<div class='flex flex-col mt-5 space-y-3 font-bold'>
-					<label for='phone'>Password</label>
+				{error.role && (
+					<p className='text-red-600 text-sm font-bold'>{error.role}</p>
+				)}
+
+				<div className='flex flex-col mt-5 space-y-3 font-bold'>
+					<label>Password</label>
 					<input
-						type='text'
+						type='password'
 						className='h-16 w-full border-2 rounded-2xl p-1'
+						onChange={(e) => setPassword(e.target.value)}
 					/>
 				</div>
+				{error.password && (
+					<p className='text-red-600 text-sm font-bold'>{error.password}</p>
+				)}
 
-				<button className='h-16 w-full bg-green-500 text-center rounded-2xl mt-10 font-bold text-white'>
+				<button
+					className='h-16 w-full bg-green-500 text-center rounded-2xl mt-10 font-bold text-white'
+					onClick={onHandleSubmit}>
 					Done
 				</button>
 				<button className='h-16 w-full border-2 text-center rounded-2xl mt-10 font-bold text-black'>
@@ -65,12 +135,14 @@ export default function CreateAccount() {
 					<p>By signing up you agree to our teams and conditions</p>
 					<p>& privacy policy</p>
 				</div>
-			</div>
-			<p className="text-xs mt-12">Already have an Account?</p>
+			</form>
+			<p className='text-xs mt-12'>Already have an Account?</p>
 			<div className='w-96 p-4 mb-10'>
-				<button className='h-16 w-full border-2 text-center rounded-2xl mt-10 font-bold text-black'>
-					Login
-				</button>
+				<Link to="/">
+					<button className='h-16 w-full border-2 text-center rounded-2xl mt-10 font-bold text-black'>
+						Login
+					</button>
+				</Link>
 			</div>
 		</div>
 	);
--- src/utils/validateInfo.js
+++ src/utils/validateInfo.js
@@ -25,3 +25,43 @@ export const validateLogin = (values) => {
     }
     return errors;
 }
+
+
+export const validateSignUp = (values) => {
+    let errors = {};
+
+    if (!values.firstName.trim()) {
+        errors.firstName = "First Name Required"
+    }
+
+    if (!values.lastName.trim()) {
+        errors.lastName = "Last Name Required"
+    }
+
+    if (!values.email) {
+        errors.email = "Email Required"
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+        errors.email = "Email is invalid"
+    }
+
+    if (!values.phoneNumber) {
+        errors.phoneNumber = "Phone Number is Required"
+    } else if (values.phoneNumber.length < 11) {
+        errors.phoneNumber = "Phone Number need to be 8 characters and above"
+    }
+
+    if(!values.userName){
+        errors.userName = "User Name is Required"
+    }
+
+    if (!values.role) {
+        errors.role = "Role is Required"
+    }
+
+    if (!values.password) {
+        errors.password = "Password is Required"
+    } else if (values.password.length < 8) {
+        errors.password = "Password need to be 8 characters and above"
+    }
+    return errors;
+}
